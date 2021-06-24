17c32b5fada633c933a9d1023c15460ae625e613
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-06-24 17:39:49 -0500

    19 - Eliminar registro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/', function () {
     ]);
 });
 
+Route::post('tags',[TagController::class,'store']);
