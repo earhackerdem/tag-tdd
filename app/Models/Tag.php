fe6fddf45a46386e8c3ade10889eeefa8326f62b
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-06-28 19:41:50 -0500

    23 - Aplicando pruebas unitarias a cambios

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,4 +10,12 @@ class Tag extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function getSlugAttribute()
+    {
+        return strtolower(
+            str_replace(' ','-',$this->name)
+        );
+    }
+
 }
